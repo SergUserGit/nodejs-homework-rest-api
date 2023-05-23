d11aa2cb4ac485a945ef6e42c6b320ca17af9b10
Sergii Podriezienko <podreenko1986@gmail.com>
2023-05-23 10:14:49 +0300

    add: connect db
    add: connect db

--- server.js
+++ server.js
@@ -1,5 +1,24 @@
-const app = require('./app')
+const app = require("./app");
+const moongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+//const DB_HOST =
+//  "mongodb+srv://Serhii:zm6pRFWChgpB6K.@cluster0.efcvmyh.mongodb.net/db-contacts";
+
+const { DB_HOST } = require("./config");
+
+moongoose.set("strictQuery", true);
+
+moongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000);
+    console.log("Database connection successful");
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
+
+//app.listen(3000, () => {
+//  console.log("Server running. Use our API on port: 3000");
+//});
