11869fc9993264efe0ccf88d8879306b31c612b4
Sergii Podriezienko <podreenko1986@gmail.com>
2023-05-25 12:11:19 +0300

    add: parol salt
    add: parol salt

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,9 @@
+const bcryptjs = require("bcryptjs");
+
 const { User } = require("../models/user");
 
 const register = async (req, res) => {
-  const { email } = req.body;
+  const { email, password } = req.body;
 
   const user = await User.findOne({ email });
 
@@ -9,7 +11,8 @@ const register = async (req, res) => {
     return res.status(409).json({ message: `Email - ${email} in use` });
   }
 
-  const newUser = await User.create(req.body);
+  const hashPassword = await bcryptjs.hash(password, 10);
+  const newUser = await User.create({ ...req.body, password: hashPassword });
 
   res.status(201).json({
     email: newUser.email,
